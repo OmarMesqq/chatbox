6aebdf8cd2a26c099aac9383c3fedee91c79d742
OmarMesqq <omarmsqt@gmail.com>
2025-05-22 19:09:43 -0300

    refactor: :fire: remove event.ts

--- src/renderer/packages/event.ts
+++
@@ -1,7 +0,0 @@
-import platform from '@/platform'
-import { allowReportingAndTrackingAtom } from '@/stores/atoms'
-import { getDefaultStore } from 'jotai'
-
-export function trackingEvent(name: string, params: { [key: string]: string } = {}) {
-  return;
-}
