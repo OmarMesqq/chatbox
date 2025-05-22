f7b05cdde8fbf95ace535f6fedd903e22f0aac47
OmarMesqq <omarmsqt@gmail.com>
2025-05-22 18:45:34 -0300

    refactor: :truck: change appId and name in capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'xyz.chatboxapp.app',
-  appName: 'Chatbox 2.0',
+  appId: 'xyz.zeroxchat.sh',
+  appName: 'Zerox Chat',
   webDir: 'release/app/dist/renderer'
 };
 
