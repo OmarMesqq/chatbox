889fe1812688b001a6e952c292974175f018ef8b
OmarMesqq <omarmsqt@gmail.com>
2025-05-22 18:28:24 -0300

    chore: add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xyz.chatboxapp.app',
+  appName: 'Chatbox 2.0',
+  webDir: 'release/app/dist/renderer'
+};
+
+export default config;
