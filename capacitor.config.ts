2fe60683fc49a325145118f7216ff990809bd073
OmarMesqq <omarmsqt@gmail.com>
2025-05-23 01:56:18 -0300

    restore package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'xyz.zeroxchat.sh',
-  appName: 'Zerox Chat',
+  appId: 'xyz.chatboxapp.ce',
+  appName: 'xyz.chatboxapp.ce',
   webDir: 'release/app/dist/renderer'
 };
 
