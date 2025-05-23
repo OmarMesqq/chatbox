8e9a7180c5b77e6491e750426c6b683bca1da97d
OmarMesqq <omarmsqt@gmail.com>
2025-05-23 13:57:43 -0300

    set appName in capacitor to Chatbox

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'xyz.chatboxapp.ce',
-  appName: 'xyz.chatboxapp.ce',
+  appName: 'Chatbox',
   webDir: 'release/app/dist/renderer'
 };
 
