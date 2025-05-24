5306dfa2decda27696180189e3b319984d12344e
OmarMesqq <omarmsqt@gmail.com>
2025-05-24 01:40:14 -0300

    fix: :bug: add cleartext true to capacitor config to allow lan models

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'xyz.chatboxapp.ce',
   appName: 'Chatbox',
-  webDir: 'release/app/dist/renderer'
+  webDir: 'release/app/dist/renderer',
+   server: {
+    cleartext: true
+  }
 };
 
 export default config;
