6e218c8688100d80a631a0e9a5783003a4b32260
OmarMesqq <omarmsqt@gmail.com>
2026-01-21 18:52:30 -0300

    fix layout in settings

--- src/renderer/pages/SettingDialog/ModelSettingTab.tsx
+++ src/renderer/pages/SettingDialog/ModelSettingTab.tsx
@@ -1,7 +1,7 @@
-import { Box, Divider } from '@mui/material'
+import { Box, Button, Divider, Typography } from '@mui/material'
 import { ModelProvider, ModelSettings } from '../../../shared/types'
-
 import OllamaSetting from './OllamaSetting'
+import { useTranslation } from 'react-i18next'
 
 interface ModelConfigProps {
   settingsEdit: ModelSettings
@@ -9,13 +9,30 @@ interface ModelConfigProps {
 }
 
 export default function ModelSettingTab(props: ModelConfigProps) {
-  const { settingsEdit, setSettingsEdit } = props
+  const { t } = useTranslation();
+  const { settingsEdit, setSettingsEdit } = props;
+  const aiProvider = settingsEdit.aiProvider;
+
   return (
-    <Box>
-      <Divider sx={{ marginTop: '10px', marginBottom: '24px' }} />      
-      {settingsEdit.aiProvider === ModelProvider.Ollama && (
-        <OllamaSetting settingsEdit={settingsEdit} setSettingsEdit={setSettingsEdit} />
-      )}
-    </Box>
+    <>
+      <Box>
+        <Typography variant="caption" className="opacity-50">
+          {t('Model Provider')}:
+        </Typography>
+        <div className="flex items-end justify-between">
+          <Button variant="contained" disableElevation>
+            <Typography className="text-left" maxWidth={200} noWrap>
+              {aiProvider || 'Unknown AI Provider'}
+            </Typography>
+          </Button>
+        </div>
+
+        <Divider sx={{ marginTop: '10px', marginBottom: '14px' }} />
+
+        {settingsEdit.aiProvider === ModelProvider.Ollama && (
+          <OllamaSetting settingsEdit={settingsEdit} setSettingsEdit={setSettingsEdit} />
+        )}
+      </Box>
+    </>
   )
 }
