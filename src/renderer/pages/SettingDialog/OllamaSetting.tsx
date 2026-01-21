d8cc72e1a2ff17d1fa7de7ad490dc60e37be4f06
OmarMesqq <omarmsqt@gmail.com>
2026-01-21 18:51:24 -0300

    remove alert

--- src/renderer/pages/SettingDialog/OllamaSetting.tsx
+++ src/renderer/pages/SettingDialog/OllamaSetting.tsx
@@ -30,40 +30,6 @@ export function OllamaHostInput(props: {
         fullWidth
         className={props.className}
       />
-      <Alert icon={false} severity="info" className="my-4">
-        {platform.type === 'web' && (
-          <p>
-            <Trans
-              i18nKey="Get better connectivity and stability with the Chatbox desktop application. <a>Download now</a>."
-              components={{
-                a: (
-                  <a
-                    className="cursor-pointer font-bold"
-                    onClick={() => {
-                      platform.openLink(`https://chatboxai.app`)
-                    }}
-                  ></a>
-                ),
-              }}
-            />
-          </p>
-        )}
-        <p>
-          <Trans
-            i18nKey="Please ensure that the Remote Ollama Service is able to connect remotely. For more details, refer to <a>this tutorial</a>."
-            components={{
-              a: (
-                <a
-                  className="cursor-pointer font-bold"
-                  onClick={() => {
-                    platform.openLink(`https://chatboxai.app/redirect_app/ollama_guide/${language}`)
-                  }}
-                ></a>
-              ),
-            }}
-          />
-        </p>
-      </Alert>
     </>
   )
 }
