cea649a87027c9d71b4fd9e12934e0a7e7204193
OmarMesqq <omarmsqt@gmail.com>
2026-01-22 11:24:05 -0300

    rearrange functions

--- src/renderer/packages/remote.ts
+++ src/renderer/packages/remote.ts
@@ -7,42 +7,6 @@ import platform from '@/platform'
 // ========== API ORIGIN 根据可用性维护 ==========
 export const API_ORIGIN = 'http://localhost:8080'
 
-/**
- * 按顺序测试 API 的可用性，只要有一个 API 域名可用，就终止测试并切换所有流量到该域名。
- * 在测试过程中，会根据服务器返回添加新的 API 域名，并缓存到本地
- */
-async function testApiOrigins() {
-  // 从缓存中获取已知的 API 域名列表
-  let pool = await cache.store.getItem<string[] | null>('api_origins').catch(() => null)
-  if (!pool) {
-    pool = []
-  }
-  // 按顺序测试 API 的可用性
-  let i = 0
-  while (i < pool.length) {
-    try {
-      const origin: string = pool[i]
-      const controller = new AbortController()
-      setTimeout(() => controller.abort(), 2000) // 2秒超时
-      const res = await ofetch<{ data: { api_origins: string[] } }>(`${origin}/api/api_origins`, {
-        signal: controller.signal,
-        retry: 1,
-      })
-      // 如果服务器返回了新的 API 域名，则更新缓存
-      if (res.data.api_origins.length > 0) {
-        pool = uniq([...pool, ...res.data.api_origins])
-      }
-      // 如果当前 API 可用，则切换所有流量到该域名
-      // API_ORIGIN = origin
-      pool = uniq([origin, ...pool]) // 将当前 API 域名添加到列表顶部
-      await cache.store.setItem('api_origins', pool)
-      return
-    } catch (e) {
-      i++
-    }
-  }
-}
-
 export interface DialogConfig {
   markdown: string
   buttons: { label: string; url: string }[]
@@ -58,6 +22,91 @@ export async function getDialogConfig(params: { uuid: string; language: string; 
     body: params,
   })
   return res['data'] || null
+}
+
+export async function webBrowsing(params: { licenseKey: string; query: string }) {
+  type Response = {
+    data: {
+      uuid?: string
+      query: string
+      links: {
+        title: string
+        url: string
+        content: string
+      }[]
+    }
+  }
+  const res = await afetch(
+    `${API_ORIGIN}/api/tool/web-search`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: params.licenseKey,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    },
+    {
+      parseChatboxRemoteError: true,
+      retry: 2,
+    }
+  )
+  const json: Response = await res.json()
+  return json['data']
+}
+
+export async function parseUserLinkPro(params: { licenseKey: string; url: string }) {
+  type Response = {
+    data: {
+      uuid: string
+      title: string
+      content: string
+    }
+  }
+  const res = await afetch(
+    `${API_ORIGIN}/api/links/parse`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: params.licenseKey,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...params,
+        returnContent: true,
+      }),
+    },
+    {
+      parseChatboxRemoteError: true,
+      retry: 2,
+    }
+  )
+  const json: Response = await res.json()
+  const storageKey = `parseUrl-${params.url}_${json['data']['uuid']}.txt`
+  if (json['data']['content']) {
+    await platform.setStoreBlob(storageKey, json['data']['content'])
+  }
+  return {
+    key: json['data']['uuid'],
+    title: json['data']['title'],
+    storageKey,
+  }
+}
+
+async function parseUserLinkFree(params: { url: string }) {
+  type Response = {
+    title: string
+    text: string
+  }
+  const res = await afetch(`https://cors-proxy.chatboxai.app/api/fetch-webpage`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(params),
+  })
+  const json: Response = await res.json()
+  return json
 }
 
 async function generateUploadUrl(params: { licenseKey: string; filename: string }) {
@@ -129,87 +178,38 @@ async function uploadAndCreateUserFile(licenseKey: string, file: File) {
   return storageKey
 }
 
-export async function parseUserLinkPro(params: { licenseKey: string; url: string }) {
-  type Response = {
-    data: {
-      uuid: string
-      title: string
-      content: string
+/**
+ * 按顺序测试 API 的可用性，只要有一个 API 域名可用，就终止测试并切换所有流量到该域名。
+ * 在测试过程中，会根据服务器返回添加新的 API 域名，并缓存到本地
+ */
+async function testApiOrigins() {
+  // 从缓存中获取已知的 API 域名列表
+  let pool = await cache.store.getItem<string[] | null>('api_origins').catch(() => null)
+  if (!pool) {
+    pool = []
+  }
+  // 按顺序测试 API 的可用性
+  let i = 0
+  while (i < pool.length) {
+    try {
+      const origin: string = pool[i]
+      const controller = new AbortController()
+      setTimeout(() => controller.abort(), 2000) // 2秒超时
+      const res = await ofetch<{ data: { api_origins: string[] } }>(`${origin}/api/api_origins`, {
+        signal: controller.signal,
+        retry: 1,
+      })
+      // 如果服务器返回了新的 API 域名，则更新缓存
+      if (res.data.api_origins.length > 0) {
+        pool = uniq([...pool, ...res.data.api_origins])
+      }
+      // 如果当前 API 可用，则切换所有流量到该域名
+      // API_ORIGIN = origin
+      pool = uniq([origin, ...pool]) // 将当前 API 域名添加到列表顶部
+      await cache.store.setItem('api_origins', pool)
+      return
+    } catch (e) {
+      i++
     }
   }
-  const res = await afetch(
-    `${API_ORIGIN}/api/links/parse`,
-    {
-      method: 'POST',
-      headers: {
-        Authorization: params.licenseKey,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        ...params,
-        returnContent: true,
-      }),
-    },
-    {
-      parseChatboxRemoteError: true,
-      retry: 2,
-    }
-  )
-  const json: Response = await res.json()
-  const storageKey = `parseUrl-${params.url}_${json['data']['uuid']}.txt`
-  if (json['data']['content']) {
-    await platform.setStoreBlob(storageKey, json['data']['content'])
-  }
-  return {
-    key: json['data']['uuid'],
-    title: json['data']['title'],
-    storageKey,
-  }
-}
-
-async function parseUserLinkFree(params: { url: string }) {
-  type Response = {
-    title: string
-    text: string
-  }
-  const res = await afetch(`https://cors-proxy.chatboxai.app/api/fetch-webpage`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(params),
-  })
-  const json: Response = await res.json()
-  return json
-}
-
-export async function webBrowsing(params: { licenseKey: string; query: string }) {
-  type Response = {
-    data: {
-      uuid?: string
-      query: string
-      links: {
-        title: string
-        url: string
-        content: string
-      }[]
-    }
-  }
-  const res = await afetch(
-    `${API_ORIGIN}/api/tool/web-search`,
-    {
-      method: 'POST',
-      headers: {
-        Authorization: params.licenseKey,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(params),
-    },
-    {
-      parseChatboxRemoteError: true,
-      retry: 2,
-    }
-  )
-  const json: Response = await res.json()
-  return json['data']
 }
